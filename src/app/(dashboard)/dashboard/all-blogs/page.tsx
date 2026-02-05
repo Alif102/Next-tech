babe4bbc28254b85d17df475a46b5dcd8255d911
Alif Ahmed <alifahmed102@gmail.com>
2026-02-05 22:15:16 +0600

    Add blog from from backend api

---
+++ src/app/(dashboard)/dashboard/all-blogs/page.tsx
@@ -0,0 +1,12 @@
+import AllBlogs from "@/components/modules/Blogs/AllBlogs";
+import React from "react";
+
+const CreateBlog = () => {
+  return (
+    <div className="w-full flex justify-center items-center">
+      <AllBlogs />
+    </div>
+  );
+};
+
+export default CreateBlog;
